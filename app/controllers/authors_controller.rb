d9ad5f21040d58f58574e7fe1554b3460e982750
George  Pianka <gpianka1@gmail.com>
2019-02-03 09:02:27 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -21,7 +21,6 @@ class AuthorsController < ApplicationController
   end
 
   def update
-    @author = Author.find(params[:id])
     if @author.update(author_params)
       redirect_to author_path(@author)
     else
